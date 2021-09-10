3a2dacd427d28f5a4f5cf89feda63dd31e9c6172
limitMe <z@limme.net>
2021-09-10 18:40:42 +0800

    Add react-router-dom support

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
 import Page from './component/Page'
 import PCICP from './PCICP';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Page>
-      <PCICP />
-    </Page>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Page>
+            <PCICP />
+          </Page>
+        </Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
