cfcbf39441a51a7db161560e88e5636982a84a39
limitMe <z@limme.net>
2021-09-11 20:53:47 +0800

    Update itsTime and forest part

--- src/invitation/forest.tsx
+++ src/invitation/forest.tsx
@@ -1,7 +1,19 @@
 import Page from '../component/Page'
 import DownArrow from '../component/DownArrow'
+import { useViewport } from '../component/Viewport'
+import { useSpring, animated } from '@react-spring/web'
 
 const Forest = (props: any) => {
+  const { width, height } = useViewport()
+  const copyAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    pause: !props.active,
+    config: {
+      duration: 600,
+    }
+  })
+
   return (
     <Page className="forest" style={{
       backgroundImage: 'url("/images/forest.jpg")',
@@ -11,6 +23,16 @@ const Forest = (props: any) => {
       {
         props.active ?
         <>
+          <animated.div style={{
+            height: height + 'px',
+            width: width + 'px',
+            position: 'absolute',
+            zIndex: 2,
+            backgroundImage: 'url("/images/forest_copy.png")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            ...copyAnimation
+          }} />
           <DownArrow delay={5000} />
         </> : null
       }
--- src/invitation/itsTime.tsx
+++ src/invitation/itsTime.tsx
@@ -1,19 +1,83 @@
 import Page from '../component/Page'
-import useSound from 'use-sound'
 import Typist from 'react-typist'
-import shipHorn from '../sound/ship_horn.mp3'
+import Particles from 'react-particles-js';
 import DownArrow from '../component/DownArrow'
+import { useViewport } from '../component/Viewport'
+import './itsTime.scss'
 
 const ItsTime = (props: any) => {
+  const { width, height } = useViewport()
+
   return (
     <Page className="its_time" style={{
       backgroundImage: 'url("/images/its_time.jpg")',
       backgroundSize: 'cover',
       backgroundPosition: 'center'
     }}>
+      <div style={{
+        height: height + 'px',
+        width: width + 'px',
+        position: 'absolute',
+        zIndex: 2,
+        backgroundImage: 'url("/images/its_time_sky.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }} />
+      <Particles
+        width={(width-4).toString() + 'px'}
+        height='300px'
+        style={{
+          height: '300px',
+          width: width + 'px',
+          position: 'absolute',
+          zIndex: 3,
+        }}
+        params={{
+          "particles": {
+              "number": {
+                  "value": 280,
+                  "density": {
+                      "enable": true,
+                      "value_area": 1500
+                  }
+              },
+              "line_linked": {
+                  "enable": true,
+                  "frequency": 0.3,
+                  "opacity": 0.1
+              },
+              "move": {
+                  "direction": "right",
+                  "speed": 0.05
+              },
+              "size": {
+                  "value": 2
+              },
+              "opacity": {
+                  "anim": {
+                      "enable": true,
+                      "speed": 1,
+                      "opacity_min": 0.3
+                  }
+              }
+          },
+          "retina_detect": true
+      }} />
       {
         props.active ?
         <>
+          <div className="text">
+            <Typist>
+              转瞬间
+              <br />
+              四千日月尽
+              <br />
+              <br />
+              恰今朝
+              <br />
+              三星在天时
+            </Typist>
+          </div>
           <DownArrow delay={5000} />
         </> : null
       }
