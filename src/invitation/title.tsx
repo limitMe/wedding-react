48f2e4f90e5189b140dd82fa1e115eaa79cc66ad
limitMe <z@limme.net>
2021-09-11 16:41:24 +0800

    Add static layout for title page

--- src/invitation/title.tsx
+++ src/invitation/title.tsx
@@ -1,11 +1,33 @@
 import Page from '../component/Page'
+import Viewport, { useViewport } from '../component/Viewport';
 
 const Title = () => {
+  const { width, height } = useViewport()
   return (
     <Page style={{
-      backgroundColor: '#f00'
+      backgroundImage: 'url("/images/title.jpg")',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center'
     }}>
-
+      <div style={{
+        height: height + 'px',
+        width: width + 'px',
+        position: 'absolute',
+        zIndex: 2,
+        backgroundImage: 'url("/images/title_copy_1.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }} />
+      <div style={{
+        height: height + 'px',
+        width: width + 'px',
+        top: 0,
+        position: 'absolute',
+        zIndex: 3,
+        backgroundImage: 'url("/images/title_copy_2.png")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }} />
     </Page>
   )
 }
