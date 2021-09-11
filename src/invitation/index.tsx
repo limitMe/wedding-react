1a917c271598b171762d9b5d3c0d5f914686d160
limitMe <z@limme.net>
2021-09-11 15:49:54 +0800

    Use fullpage.js to achieve fullpage scrolling

--- src/invitation/index.tsx
+++ src/invitation/index.tsx
@@ -1,4 +1,5 @@
-import { useIsMobile, useViewport } from '../component/Viewport';
+import { useIsMobile } from '../component/Viewport';
+import ReactFullpage from '@fullpage/react-fullpage';
 import PCPage from '../PC';
 import Title from './title'
 import Beginning from './beginning'
@@ -9,10 +10,23 @@ const Invitation = () => {
     <>
       {
         isMobile ?
-        <>
-          <Title />
-          <Beginning />
-        </> :
+        <ReactFullpage
+          licenseKey = {'YOUR_KEY_HERE'}
+          scrollingSpeed = {1000}
+          // @ts-ignore
+          render={({ state, fullpageApi }) => {
+            return (
+              <ReactFullpage.Wrapper>
+                <div className="section">
+                  <Title />
+                </div>
+                <div className="section">
+                <Beginning />
+                </div>
+              </ReactFullpage.Wrapper>
+            );
+          }}
+        /> :
         <PCPage></PCPage>
       }
     </>
