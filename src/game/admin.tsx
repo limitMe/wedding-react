f41f1e6f552ae237006318cb965919a239d72792
limitMe <z@limme.net>
2021-09-18 18:37:21 +0800

    Add admin page

--- src/game/admin.tsx
+++ src/game/admin.tsx
@@ -1,33 +1,106 @@
 import { useState, useEffect } from 'react';
-import ReactFullpage from '@fullpage/react-fullpage';
+import { List, Carousel } from 'antd-mobile';
+import 'antd-mobile/lib/list/style/css';
+import 'antd-mobile/lib/carousel/style/css';
 import { get } from './Fetch';
+import Emoji from './Emoji';
+
+const Item = List.Item;
+const Brief = Item.Brief;
+
+const NumberPanel = (props: { title: string, numberLine: string, backgroundColor: string }) => {
+  return (
+    <a
+      style={{
+        display: 'inline-block',
+        width: '100%',
+        height: '120px',
+        textAlign: 'center',
+        backgroundColor: props.backgroundColor
+        }}>
+      <p style={{fontSize: '16px', fontWeight: 'bold'}}>{props.title}</p>
+      <p style={{fontSize: '24px', fontWeight: 'bold'}}>{props.numberLine}</p>
+    </a>
+  )
+}
 
 const Admin = () => {
 
+  const [data, setData] = useState<any>({});
+
   const fetchAdminData = async () => {
     const result = await get('/admin', 'admin');
-    console.log(result)
+    setData(result);
   }
 
   useEffect(() => {
     fetchAdminData();
-  });
+  }, []);
 
-  return  <ReactFullpage
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000}
-    // @ts-ignore
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section" style={{
-            backgroundColor: '#0ff'
-          }}>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
+  console.log(data)
+  return(
+    data && data.success ?
+    <div>
+      <Carousel
+        autoplay={true}
+        infinite
+      >
+        <NumberPanel title="金奖候选人" numberLine={data.totalGoldenNum} backgroundColor="#DFB55A" />
+        <NumberPanel title="银奖候选人" numberLine={data.totalSilverNum} backgroundColor="#BABABA" />
+        <NumberPanel title="第二关线索还剩" numberLine={data.leftToken2} backgroundColor="#904020" />
+        <NumberPanel title="第三关线索还剩" numberLine={data.leftToken3} backgroundColor="#3FB57A" />
+      </Carousel>
+      <List renderHeader={() => '金奖嘉宾'} className="my-list">
+        {
+          // @ts-ignore
+          data.goldenWinners && data.goldenWinners.length > 0 ? data.goldenWinners.map(guest => {
+            return (
+              <Item multipleLine extra={`${guest.correctAnswer}:[${guest.answer1},${guest.answer2}]`} key={guest.name}>
+                { guest.name }
+                <Brief>
+                  { guest.number }
+                </Brief>
+              </Item>
+            )
+          }) : <Item extra="" key="none"> 暂无人完成挑战 </Item>
+        }
+      </List>
+      <List renderHeader={() => '银奖嘉宾'} className="my-list">
+        {
+          // @ts-ignore
+          data.silverWinners && data.silverWinners.length > 0 ? data.silverWinners.map(guest => {
+            return (
+              <Item multipleLine extra={`${guest.correctAnswer}:[${guest.answer1},${guest.answer2}]`} key={guest.name}>
+                { guest.name }
+                <Brief>
+                  { guest.number }
+                </Brief>
+              </Item>
+            )
+          }) : <Item extra="" key="none"> 暂无人完成挑战 </Item>
+        }
+      </List>
+      <List renderHeader={() => '陪跑嘉宾'} className="my-list">
+        {
+          // @ts-ignore
+          data.peipaoGuests ? data.peipaoGuests.map(guest => {
+            return (
+              <Item multipleLine extra={`${guest.correctAnswer}:[${guest.answer1},${guest.answer2}]`} key={guest.name}>
+                { guest.name }
+                <Brief>
+                  线索2: {
+                    guest.character2Revealed ? <Emoji symbol="✅" /> : <Emoji symbol="❌" />
+                  } 线索3: {
+                    guest.character3Revealed ? <Emoji symbol="✅" /> : <Emoji symbol="❌" />
+                  }
+                </Brief>
+              </Item>
+            )
+          }) : null
+        }
+      </List>
+    </div> : null
+  )
 }
 
 export default Admin
